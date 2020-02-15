80d32b05813e36ef5bb8928abbdbdbb945d50203
inkwon3 <60799735+inkwon3@users.noreply.github.com>
2020-02-16 01:08:01 +0900

    Update Chart.java

--- src/POS/Chart.java
+++ src/POS/Chart.java
@@ -7,11 +7,8 @@ import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
 
 import javax.swing.*;
-import java.awt.*;
 import java.sql.SQLException;
 
 public class Chart extends JFrame {
